91eaa42ca594d70dfba30000eab79bdc2c0288bf
Josh Caithness <88576573+BendyCabbage@users.noreply.github.com>
2023-08-17 22:38:55 +1000

    Extra formatting

--- backend.py
+++ backend.py
@@ -2,6 +2,9 @@
 import csv, re
 from dataclasses import dataclass
 
+output_file = "summary.txt"
+transaction_output = "transactions.txt"
+
 @dataclass
 class Transaction:
     date: str
@@ -9,10 +12,16 @@ class Transaction:
     transaction_name: str
     balance: float
 
+
 def main():
     transactions = parse_csv("transactions.csv")
     categories = parse_transactions(transactions)
-    parsed_categories = parse_categories(categories)
+
+    summary = create_summary(categories)
+    write_to_file(summary, output_file)
+
+    transaction_string = create_transactions_string(categories)
+    write_to_file(transaction_string, transaction_output)
 
 def parse_csv(csv_file):
     try:
@@ -31,7 +40,11 @@ def parse_csv(csv_file):
         transaction_name = row[2]
         balance = float(row[3])
 
-        transactions.append(Transaction(date=date, amount=amount, transaction_name=transaction_name, balance=balance))
+        new_transaction = Transaction(date=date, amount=amount, transaction_name=transaction_name, balance=balance)
+
+        new_transaction = format_transaction(new_transaction)
+
+        transactions.append(new_transaction)
 
     return transactions
 
@@ -67,7 +80,7 @@ def is_income(transaction: Transaction) -> bool:
     return transaction.amount > 0
 
 def is_eating_out(transaction: Transaction) -> bool:
-    potential_strings = ["zambrero", "mcdonalds", "dominos", "guzman", "boost juice"]
+    potential_strings = ["zambrero", "mcdonalds", "dominos", "guzman", "boost juice", "mad mex", "subway", "kfc", "sharetea", "chatime"]
     return any(s in transaction.transaction_name.lower() for s in potential_strings)
 
 def is_transport(transaction: Transaction) -> bool:
@@ -75,11 +88,53 @@ def is_transport(transaction: Transaction) -> bool:
     return any(s in transaction.transaction_name.lower() for s in potential_strings)
 
 def is_rent(transaction: Transaction) -> bool:
-    return transaction.amount > 150 and "unswrandwick" in transaction.transaction_name.replace(" ","").lower()
+    return transaction.amount < -150 and "unswrandwick" in transaction.transaction_name.replace(" ","").lower()
 
-def parse_categories(categories):
+def format_transaction(transaction: Transaction):
+    date_match = re.search(r'([0-9]{2}/[0-9]{2}/[0-9]{4})$', transaction.transaction_name)
+    if date_match:
+        transaction.date = date_match.group(1)
+    transaction.transaction_name = re.sub(r'( [A-Z]{2} )?[A-Z]{3} Card.*', r'', transaction.transaction_name)
+    return transaction
+
+def create_summary(categories) -> str:
+    summary_string = "Summary:\n"
+
     for category in categories:
-        print(f"{category}: {categories[category]}")
+        summary_string += category + ":\n"
+        category_sum = round(sum([i.amount for i in categories[category]]),2)
+        num_transactions = len(categories[category])
+        summary_string += f"\t{format_amount(category_sum)}\n"
+
+        summary_string += f"\tNumber of Transactions: {num_transactions}\n\n"
+
+    return summary_string
+
+def format_amount(amount):
+    if amount < 0:
+        return  f"-${-amount}"
+    else:
+        return f"${amount}"
+
+def custom_key(transaction: Transaction):
+    date = transaction.date.replace("/", "")
+    return int(date)
+
+def create_transactions_string(categories) -> str:
+    transaction_string = "Transactions:\n"
+    for i in categories:
+        transaction_string += i + ":\n"
+        for t in sorted(categories[i], key=custom_key):
+            transaction_string += "\t" + transaction_to_str(t) + "\n"
+
+    return transaction_string
+
+def transaction_to_str(transaction: Transaction) ->str:
+    return f"{transaction.date}: {format_amount(transaction.amount)}, {transaction.transaction_name}"
+
+def write_to_file(contents, filename):
+    with open(filename, "w") as file:
+        file.write(contents)
 
 if __name__ == "__main__":
     main()
